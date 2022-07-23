44724c55cead0be76b30248ff84f33c6de82056b
Huan Silva Lima <huansilvalima@gmail.com>
2022-07-22 22:00:17 -0300

    adicionado-projeto-com-filtros-funcionando

--- src/components/Filtros.js
+++ src/components/Filtros.js
@@ -1,10 +1,38 @@
-import React from "react";
+import { DivPaiFiltros } from "../style";
 
-function Filtros(){
+
+function Filtros(props){
 
 
     return(
         <DivPaiFiltros>
+            <h3>Filtros</h3>
+
+            <label>Busca por nome</label>
+
+            <input 
+                placeholder="Pesquisa"
+                value={props.busca}
+                onChange={(ev)=>{props.setBusca(ev.target.value)}}
+            /> 
+
+            <label>Valor minimo</label>
+
+            <input 
+                placeholder="Valor mínimo"
+                type="number"
+                value={props.precoMin}
+                onChange={(ev)=>{props.setPrecoMin(ev.target.value)}}
+                />
+
+            <label>Valor máximo</label>
+
+            <input 
+                placeholder="Valor máximo"
+                type="number"
+                value={props.precoMax}
+                onChange={(ev)=>{props.setPrecoMax(ev.target.value)}}
+            />       
 
         </DivPaiFiltros>
     )
